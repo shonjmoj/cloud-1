f91d1f0f7301cb825ba2dffa0a779253f04ab2e3
IssaMDounejjaR <otaku6issam@gmail.com>
2024-10-11 16:06:50 +0100

    update inception

--- inception/srcs/requirements/mariadb/srcs/setup.sh
+++ inception/srcs/requirements/mariadb/srcs/setup.sh
@@ -6,7 +6,8 @@ then
 	service mariadb start
 
 	mysql -u root -e "CREATE DATABASE ${WORDPRESS_DATABASE_NAME};"
-	mysql -u root -D $WORDPRESS_DATABASE_NAME < /tmp/wordpress.sql
+	sed 's/iounejja.42.fr/'"$DOMAIN"'/g' /tmp/wordpress.sql > /tmp/setup.sql
+	mysql -u root -D $WORDPRESS_DATABASE_NAME < /tmp/setup.sql
 	mysql -u root -e "CREATE USER '${MARIADB_USER}'@'%' IDENTIFIED BY '${MARIADB_USER_PASSORD}';"
 	mysql -u root -e "GRANT ALL PRIVILEGES ON *.* TO '${MARIADB_USER}'@'%';"
 	mysql -u root -e "CREATE USER '${MARIADB_USER}'@'localhost' IDENTIFIED BY '${MARIADB_USER_PASSORD}';"
